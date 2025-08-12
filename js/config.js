4052ca5ceaf8b37d0265da3a674e0875047ededb
Anu-Jose-1997 <anujinnyjose@gmail.com>
2025-08-13 02:53:21 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,11 +1,12 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_0Hx3OvDuu', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '7keh9ipfkr22hpq9rtb2ln5guj', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_YXbgSTYjx', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '5ve8q0dthvb016010nqc3gk16k', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
 
