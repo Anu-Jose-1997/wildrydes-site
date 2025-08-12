15c19deff4bf0b52b46360ffffea0f4c4ee23b2e
Anu-Jose-1997 <anujinnyjose@gmail.com>
2025-08-13 03:25:52 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -5,8 +5,9 @@ window._config = {
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://lspy4s6vd1.execute-api.us-east-1.amazonaws.com/Dev' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
 
 
